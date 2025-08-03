6ef788e81f1ac973abedab022953976391944b3d
7ODACOD <abdallhantrosm@gmail.com>
2025-08-03 22:21:18 +0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,9 @@
+// Smooth scroll to sections when clicking on links
+document.querySelectorAll('a[href^="#"]').forEach(link => {
+  link.addEventListener('click', function(e) {
+    e.preventDefault();
+    document.querySelector(this.getAttribute('href')).scrollIntoView({
+      behavior: 'smooth'
+    });
+  });
+});
